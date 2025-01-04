0315a6eb1cf59a4f4f0c6dffd1636d7179959366
cheng <hczshd@163.com>
2025-01-04 12:02:11 +0800

    :zap: 添加短链缓存

--- tiny-url-app-backend/src/main/java/fun/powercheng/url/tiny/util/CustomRedisTemplate.java
+++ tiny-url-app-backend/src/main/java/fun/powercheng/url/tiny/util/CustomRedisTemplate.java
@@ -1,11 +1,15 @@
 package fun.powercheng.url.tiny.util;
 
+import fun.powercheng.url.tiny.config.CacheConfig;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+
+import static fun.powercheng.url.tiny.util.ProjectConstants.REDIS_KEY_APP_PREFIX;
 
 /**
  * Created by PowerCheng on 2025/1/2.
@@ -16,16 +20,37 @@ public class CustomRedisTemplate {
 
     private final ReactiveStringRedisTemplate redisTemplate;
 
+    private final CacheConfig cacheConfig;
+
+    private Duration configDuration;
+
+    @PostConstruct
+    public void init() {
+        configDuration = Duration.of(cacheConfig.getCacheDuration(), cacheConfig.getDurationUnit());
+    }
+
+    /**
+     * 缓存 key 返回 value，使用默认的 fun.powercheng.url.tiny.config.CacheConfig 配置的过期时长
+     *
+     * @param key redis key
+     * @param value redis value
+     * @return 刚刚缓存的 value
+     */
+    public Mono<String> saveValue(String key, String value) {
+        return redisTemplate.opsForValue().set(REDIS_KEY_APP_PREFIX +key, value, configDuration)
+                .thenReturn(value);
+    }
+
     public Mono<String> saveValue(String key, String value, Duration duration) {
-        return redisTemplate.opsForValue().set(key, value, duration)
+        return redisTemplate.opsForValue().set(REDIS_KEY_APP_PREFIX +key, value, duration)
                 .thenReturn(value);
     }
 
     public Mono<String> getValue(String key) {
-        return redisTemplate.opsForValue().get(key);
+        return redisTemplate.opsForValue().get(REDIS_KEY_APP_PREFIX + key);
     }
 
     public Mono<Boolean> deleteValue(String key) {
-        return redisTemplate.opsForValue().delete(key);
+        return redisTemplate.opsForValue().delete(REDIS_KEY_APP_PREFIX +key);
     }
 }
--- tiny-url-app-backend/src/test/java/fun/powercheng/url/tiny/TinyUrlAppBackendApplicationTests.java
+++ tiny-url-app-backend/src/test/java/fun/powercheng/url/tiny/TinyUrlAppBackendApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
@@ -28,6 +29,21 @@ class TinyUrlAppBackendApplicationTests {
                 redisTemplate.deleteValue(testKey).subscribe(Assertions::assertTrue);
             });
         });
+
+
+        Mono<String> asd = redisTemplate.getValue("asd").doOnSuccess(res -> {
+            System.out.println("获取到值了 " + res);
+        });
+
+        asd.switchIfEmpty(getRes()).block();
+
+        System.out.println("jieshu");
+
+    }
+
+    private Mono<String> getRes() {
+        System.out.println("empty");
+        return Mono.just("1");
     }
 
 }
